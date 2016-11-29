9f268e7b6b100f344377cca42ea7bfe625b001dd
Igor Dorovskikh <i.dorovskikh@agilefusion.com>
2016-11-28 18:34:00 -0800

    day1 completed

---
+++ DepozaUITests/DepozaUITests.swift
@@ -0,0 +1,47 @@
+//
+//  DepozaUITests.swift
+//  DepozaUITests
+//
+//  Created by Igor Dorovskikh on 11/28/16.
+//  Copyright © 2016 Ivan Magda. All rights reserved.
+//
+
+import XCTest
+
+class DepozaUITests: XCTestCase {
+    
+    let app = XCUIApplication()
+
+    override func setUp() {
+        super.setUp()
+        continueAfterFailure = false
+        app.launchArguments = ["isUITesting"]
+        app.launch()
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testAddingNewExpense() {
+        
+        app.buttons["add_button"].tap()
+        
+        let tablesQuery = app.tables
+        tablesQuery.textFields["enter_amount"].tap()
+        tablesQuery.textFields["enter_amount"].typeText("100")
+        
+        tablesQuery.staticTexts["Clothes"].tap()
+        
+        let enterDescriptionTextField = tablesQuery.textFields["enter_description"]
+        enterDescriptionTextField.tap()
+        enterDescriptionTextField.typeText("socks")
+        app.navigationBars["Add Expense"].buttons["Done"].tap()
+        
+        let totalExpensesAmountStaticText = app.tables.staticTexts["total_expenses_amount"].label
+        XCTAssert(totalExpensesAmountStaticText == "100","actula amount is \(totalExpensesAmountStaticText)")
+        
+    }
+    
+}
